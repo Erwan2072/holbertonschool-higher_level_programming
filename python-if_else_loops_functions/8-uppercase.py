dac37a7fdc1161041ed9b92b5c12a6b55c8c28b4
Erwan2072 <erwan.lebreton@hotmail.fr>
2024-05-08 10:47:33 +0200

    modif function

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -2,7 +2,7 @@
 def uppercase(str):
     new_str = ""
     for char in str:
-        if ('a') <= ord(char) <= ('z'):
+        if 97 <= ord(char) <= 122:
             new_str += chr(ord(char) - 32)
         else:
             new_str += char
