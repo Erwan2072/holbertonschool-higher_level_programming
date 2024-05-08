065ecb1dc5a502ffd3d55bb40b1086e2f52b761a
Erwan2072 <erwan.lebreton@hotmail.fr>
2024-05-08 09:49:47 +0200

    coorection function

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -4,10 +4,11 @@ number = random.randint(-10000, 10000)
 
 last_digit = abs(number) % 10
 
-if last_digit == 0:
-    print("Last digit of", number, "is", last_digit, "and is 0")
-elif last_digit > 5:
-    print("Last digit of", number, "is", last_digit, "and is greater than 5")
-else:
-    print("Last digit of", number, "is",
-          last_digit, "and is less than 6 and not 0")
+if last_digit > 5:
+    str = 'Last digit of {0} is {1} and is greater than 5'
+elif last_digit < 6 and last_digit != 0:
+    str = 'Last digit of {0} is {1} and is less than 6 and not 0'
+elif last_digit == 0:
+    str = 'Last digit of {0} is {1} and is 0'
+
+print(str.format(number, last_digit))
