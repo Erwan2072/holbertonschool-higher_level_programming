b84d9f895ab2adbecf818881b5ded0ca54637666
Erwan2072 <erwan.lebreton@hotmail.fr>
2024-05-09 10:53:44 +0200

    retour de mon premier code

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
-from sys import argv
 
 if __name__ == "__main__":
-    if argv_len <= 1:
-        print("{} arguments.".format(len(argv) - 1))
-    elif argv_len == 2:
-        print("{} argument:".format(len(argv) - 1))
-        print("1: {}".format(argv[1]))
-    else:
-        print("{} arguments:".format(len(argv) - 1))
-        for i in range(1, len(argv)):
-            print("{}: {}".format(i, argv[i]))
+    import sys
+    nb_arg = len(sys.argv) - 1
+
+    if nb_arg == 0:
+        print("0 arguments.")
+    if nb_arg == 1:
+        print("1 argument:")
+    if nb_arg > 1:
+        print("{} arguments:".format(nb_arg))
+
+    for i in range(1, nb_arg + 1):
+        print("{}: {}".format(i, sys.argv[i]))
