c161ef64222792279fd5a4d08acbced4c6de0602
Erwan2072 <erwan.lebreton@hotmail.fr>
2024-05-09 10:01:28 +0200

    write et correction files with pycodestyle and compil

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+from sys import argv
+
+if __name__ == "__main__":
+    argv_len = len(argv)
+
+    if argv_len <= 1:
+        print("{} arguments.".format(len(argv) - 1))
+    elif argv_len == 2:
+        print("{} argument:".format(len(argv) - 1))
+        print("1: {}".format(argv[1]))
+    else:
+        print("{} arguments:".format(len(argv) - 1))
+        for i in range(1, len(argv)):
+            print("{}: {}".format(i, argv[i]))
