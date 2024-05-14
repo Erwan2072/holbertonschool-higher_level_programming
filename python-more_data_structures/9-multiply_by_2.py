e7cfc1c1034f48d6df1abb1337710b572c1a0ded
Erwan2072 <erwan.lebreton@hotmail.fr>
2024-05-14 16:21:49 +0200

    print function

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,3 +1,5 @@
-#!/ur/bin/python3
-def multiply_by_2(a_dictionary):
-    return {cle: valeur * 2 for cle, valeur in a_dictionary.items()}
+#!/usr/bin/python3
+new_dictionary = {}
+for key, value in a_dictionary.items():
+    new_dictionary[key] = value * 2
+    return new_dictionary
