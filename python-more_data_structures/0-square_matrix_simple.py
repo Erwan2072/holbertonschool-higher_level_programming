a46191b43d209a0086d5fb8786421c01a568ca1a
Erwan2072 <erwan.lebreton@hotmail.fr>
2024-05-14 14:55:07 +0200

    modif function

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    result = []
-
+    new_matrix = []
     for row in matrix:
         new_row = []
         for element in row:
             new_row.append(element ** 2)
-        result.append(new_row)
-
-    return result
+        new_matrix.append(new_row)
+    return new_matrix
