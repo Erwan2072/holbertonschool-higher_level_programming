28a435d050776bff5a8e370ecf8bcef127c26292
Erwan2072 <erwan.lebreton@hotmail.fr>
2024-05-17 07:40:11 +0200

    print function

---
+++ python-classes/2-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" contain python code """
+
+class Square:
+    """ contain Private instance attribute: size"""
+
+    def __init__(self, size=0):
+        if not type(size) is int:
+            raise Exception("size must be an integer")
+        if int(size) < 0:
+            raise Exception("size must be >= 0 ")
+        self.__size = size
