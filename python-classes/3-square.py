b342af4d356f6f11cb599e34005b96e25aa1606b
Erwan2072 <erwan.lebreton@hotmail.fr>
2024-05-17 07:52:24 +0200

    print function

---
+++ python-classes/3-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" contain python code """
+
+class Square:
+    """ contain Private instance attribute: size"""
+
+    def __init__(self, size=0):
+        """ iniitialize size = 0"""
+        if not type(size) is int:
+            raise Exception("size must be an integer")
+        if int(size) < 0:
+            raise Exception("size must be >= 0 ")
+        self.__size = size
+
+    def area(self):
+        """calcul and return surface per sqare metre"""
+        return self.__size ** 2
