2604de46904056d9bf6e3529e380e8da96963319
Erwan2072 <erwan.lebreton@hotmail.fr>
2024-05-21 13:06:50 +0200

    modif function

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -6,30 +6,39 @@ class Rectangle:
     """contain a simple class of rectangle"""
 
     def __init__(self, height=0, width=0):
-        self.width = width
-        self.height = height
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = width
+
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = height
 
         @property
         def width(self):
             return self.__width
 
         @width.setter
-        def width(self):
+        def width(self, value):
             if not isinstance(width, int):
                 raise TypeError("width must be an integer")
             if width < 0 :
                 raise ValueError("width must be >= 0")
-            self.__width = width
+            self.__width = value
 
         @property
         def height(self):
             return self.__height
 
         @height.setter
-        def height(self):
+        def height(self, value):
             if not isinstance(height, int):
                 raise TypeError("height must be an integer")
             if height < 0 :
                 raise ValueError("height must be >= 0")
-            self.__height = height
+            self.__height = value
 
