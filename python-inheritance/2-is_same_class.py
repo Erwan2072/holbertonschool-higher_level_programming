25772c43bbcf478df2ff198757be6999a6630f37
Erwan2072 <erwan.lebreton@hotmail.fr>
2024-05-22 13:09:07 +0200

    print function

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""function check if obj is class"""
+
+
+def is_same_class(obj, a_class):
+    """check if object is lass"""
+    if type(obj) is a_class:
+        return True
+    else:
+        return False
