0f13eea93de6329c36dbd8fb274c76ddd59afd4f
Erwan2072 <erwan.lebreton@hotmail.fr>
2024-05-23 07:26:01 +0200

    print function

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" class inheritance BaseGeomety"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    "class rectangle inherit of BaseGeometry"
+    def __init__(self, width, height):
+        self.__width = width
+        self.__height = height
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        
