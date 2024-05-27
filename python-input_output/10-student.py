1f6fea027daeb8fb92430f5ae7e296ff8cd4cf5c
Erwan2072 <erwan.lebreton@hotmail.fr>
2024-05-27 14:24:27 +0200

    print function

---
+++ python-input_output/10-student.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Student to JSON"""
+
+
+class Student:
+    """class student"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Retrieves a dictionary representation of a Student instance.
+
+        Args:
+            attrs (list): List of attribute names to retrieve. If None, all attributes are retrieved.
+
+        Returns:
+            dict: A dictionary representation of the Student instance.
+        """
+        if attrs is None:
+           return self.__dict__
+        else:
+            result = {}
+            for attr in attrs:
+                if hasattr(self, attr):
+                    result[attr] = getattr(self, attr)
+            return result
