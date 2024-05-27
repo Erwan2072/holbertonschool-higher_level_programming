fd7736a15f55315be75a222c8cf3921d96b2f021
Erwan2072 <erwan.lebreton@hotmail.fr>
2024-05-27 16:41:05 +0200

    print function

---
+++ python-serialization/task_00_basic_serialization.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Basic Serialization"""
+
+import json
+
+
+def serialize_and_save_to_file(data, filename):
+    """
+    to serialize and save data to the specified file
+    """
+    with open(filename, 'w') as file:
+        file.write(json.dumps(data))
+
+def load_and_deserialize(filename):
+    """deserialize data from the specified file"""
+    with open(filename, 'r') as file:
+        return json.load(file)
