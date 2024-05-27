670926497a9c65bce39996ce2a507cb720770d38
Erwan2072 <erwan.lebreton@hotmail.fr>
2024-05-27 15:58:29 +0200

    correction

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -7,6 +7,6 @@ def read_file(filename=""):
         Reads and prints the content of a file line by line.
     """
 
-    with open(filename, "read", encoding="utf-8") as file:
+    with open(filename, "r", encoding="utf-8") as file:
         for line in file:
             print(line, end="")
