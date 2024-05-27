5f245543b171e8675c92afccff80a7cea0b31d1b
Erwan2072 <erwan.lebreton@hotmail.fr>
2024-05-27 12:57:45 +0200

    print function

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Load, add, save"""
+
+import sys
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+
+
+def add_arguments_to_list(filename, *args):
+    try:
+        existing_list = load_from_json_file(filename)
+    except FileNotFoundError:
+        existing_list = []
+
+    new_list = existing_list + list(args)
+    save_to_json_file(new_list, filename)
+
+if __name__ == "__main__":
+    filename = "add_item.json"
+    arguments = sys.argv[1:]  # Exclude the script name from the arguments
+    add_arguments_to_list(filename, *arguments)
