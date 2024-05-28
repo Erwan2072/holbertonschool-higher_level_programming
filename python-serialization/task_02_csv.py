83e56d3a6ab9552197179f556d8cb126f5e8e4f1
Erwan2072 <erwan.lebreton@hotmail.fr>
2024-05-28 13:14:11 +0200

    correction

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -5,21 +5,24 @@ import csv
 import json
 
 
-def convert_csv_to_json(csv):
+def convert_csv_to_json(csv_file):
     """Converts data from a CSV file to a JSON file."""
     try:
         """Initialize an empty list to store the CSV data"""
+        data = []
 
-        with open(csv, "r", newline='') as csv_file:
-            """Create a CSV DictReader object"""
+        with open(csv, "r") as csv_file:
+            """Create a CSV DictRead object"""
             csv_read = csv.DictReader(csv_file)
-            data = [ row for row in csv_read]
+
+            for row in csv_read:
+                data.append(row)
 
         """ write the json file"""
-        with open('data.json', 'w') as json_file:
+        with open("data.json", "w") as json_file:
             json.dump(data, json_file, indent=4)
-
         return True
+
     except FileNotFoundError:
         return False
     except Exception as e:
