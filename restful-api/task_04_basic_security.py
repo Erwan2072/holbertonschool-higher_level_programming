a331b338f51e00226196cbf7de6596e253edbace
Erwan2072 <erwan.lebreton@hotmail.fr>
2024-05-30 16:33:26 +0200

    modif

--- restful-api/task_04_basic_security.py
+++ restful-api/task_04_basic_security.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Develop a Simple API using Python with Flask"""
 
-from flask import Flask, jsonify, request, logging
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -16,17 +16,17 @@ def home():
     """Home route to welcome users to the Flask API."""
     return "Bienvenue sur l'API Flask !"
 
-@app.route('/data', strict_slashes=False)
+@app.route('/data')
 def get_usernames():
     """Route to get the list of all usernames."""
     return jsonify(list(users.keys()))
 
-@app.route('/status', strict_slashes=False)
+@app.route('/status')
 def status():
     """Route to check the status of the API."""
     return "OK"
 
-@app.route('/users/<username>', strict_slashes=False)
+@app.route('/users/<username>')
 def get_user(username):
     """Route to get the details of a specific user by their username."""
     user = users.get(username)
@@ -53,28 +53,4 @@ def add_user():
     return jsonify({"message": "Utilisateur ajouté", "user": user})
 
 if __name__ == "__main__":
-
-    host = '0.0.0.0'
-    port = 5000
-
-    """Enable debug mode for development"""
-    debug = True
-
-    """Set up logging"""
-    logging.basicConfig(level=logging.INFO)
-    logger = logging.getLogger(__name__)
-
-    """Print a message to indicate the server is starting"""
-    logger.info(f"Starting Flask server at http://{host}:{port}/")
-
-    """Additional configuration options"""
-    app.config['ENV'] = 'development'
-    app.config['DEBUG'] = debug
-    app.config['TESTING'] = False
-
-    """Log configuration details"""
-    logger.info(f"Configuration - ENV: {app.config['ENV']}, DEBUG:
-                {app.config['DEBUG']}, TESTING: {app.config['TESTING']}")
-
-    """Run the Flask app"""
-    app.run(host=host, port=port, debug=debug)
+    app.run()
