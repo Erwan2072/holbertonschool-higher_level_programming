2d3562b8488484c87c276de2fc85d2384abda321
Erwan2072 <erwan.lebreton@hotmail.fr>
2024-06-18 15:32:09 +0200

    print script file sql

---
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,7 @@
+-- Import the database dump of hbtn_0d_tvshows
+-- script that lists all shows contained in the database hbtn_0d_tvshows.
+
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.genres_id
+ORDER BY tv_shows.titles, tc_show_genres.genre_id;
