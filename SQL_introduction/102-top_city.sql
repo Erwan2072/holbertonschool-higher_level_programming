0d5687dee5eed04d906e11497e7cb3df6d993c67
Erwan2072 <erwan.lebreton@hotmail.fr>
2024-06-19 10:05:18 +0200

    mise en page

--- SQL_introduction/102-top_city.sql
+++ SQL_introduction/102-top_city.sql
@@ -1,7 +1,2 @@
 -- script that displays the top 3 of cities temperature during July and August ordered by temperature
-SELECT city, AVG(temperature) AS avg_temp
-FROM temperatures
-WHERE month IN ('July', 'August')
-GROUP BY city
-ORDER BY avg_temp DESC
-LIMIT 3;
+SELECT city, AVG(temperature) AS avg_temp FROM temperatures WHERE month IN ('July', 'August') GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
