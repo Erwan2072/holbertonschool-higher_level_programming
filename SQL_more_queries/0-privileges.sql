79432a171e6efc6e7e90b37d545fb6aad5e53515
Erwan2072 <erwan.lebreton@hotmail.fr>
2024-06-19 09:17:30 +0200

    correction

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,14 +1,3 @@
--- Create the users if they do not already exist
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
-
--- Grant all privileges to the users
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';
-
--- Flush privileges to ensure that all changes take effect
-FLUSH PRIVILEGES;
-
 -- List all privileges for the users
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
