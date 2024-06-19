54d6ac9fdf2cf5f8a653b88ce2ff97401cfbe7d2
Erwan2072 <erwan.lebreton@hotmail.fr>
2024-06-19 09:15:36 +0200

    correction

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,7 +1,5 @@
--- Import the database dump of hbtn_0d_tvshows
--- script that lists all shows contained in the database hbtn_0d_tvshows.
-
+-- Show all shows with their genre_id. If a show doesn't have a genre, it should still be shown, but with a NULL genre_id.
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.genres_id
-ORDER BY tv_shows.titles, tc_show_genres.genre_id;
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
