3aad2d99a6c4193abd281c352bfe07a192651096
Erwan2072 <erwan.lebreton@hotmail.fr>
2024-06-20 11:14:11 +0200

    ajout commentaire

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python3
+"""
+This module connects to a MySQL database and retrieves
+all states sorted by id in ascending order.
+"""
 
 import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
+    """
+    Connects to a MySQL server running on localhost at port 3306.
+    Takes three arguments: mysql username, mysql password, and database name.
+    Executes a query to fetch all states from the database and prints each row.
+    """
     db = MySQLdb.connect(host="localhost",
                          user=argv[1],
                          passwd=argv[2],
