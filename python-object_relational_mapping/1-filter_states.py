0ee4b54416267507b06709d3db5d8cf067cfcbef
Erwan2072 <erwan.lebreton@hotmail.fr>
2024-06-20 12:42:11 +0200

    ajout comment file

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,10 +1,20 @@
+
 #!/usr/bin/python3
+"""
+This module connects to a MySQL database and retrieves
+all states with names starting with 'N', sorted by id in ascending order.
+"""
+
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3])
+
+    db = MySQLdb.connect(host="localhost",
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3])
+    
     cur = db.cursor()
     cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
     rows = cur.fetchall()
