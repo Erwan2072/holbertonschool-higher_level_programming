acebc190c10ddd624603df4ff998ad32588025e6
Erwan2072 <erwan.lebreton@hotmail.fr>
2024-06-21 08:56:49 +0200

    correction pycodestyle

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -7,10 +7,12 @@ import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost",
-                         user= argv[1],
-                         passwd= argv[2],
-                         db= argv[3])
+    db = MySQLdb.connect(
+        host="localhost",
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3])
+
     cur = db.cursor()
     cur.execute("""SELECT * FROM states
                  WHERE BINARY name LIKE '{}%'
