7d170d711f31463cbe18a8c4e139f35987f36c26
Erwan2072 <erwan.lebreton@hotmail.fr>
2024-06-21 08:54:24 +0200

    correction pycodestyle

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/python3
 """
     script that lists all states with a name starting with N
@@ -8,11 +7,11 @@ import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-
-    db = MySQLdb.connect(host="localhost",
-                         user= argv[1],
-                         passwd= argv[2],
-                         db= argv[3])
+    db = MySQLdb.connect(
+        host="localhost",
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3])
 
     cur = db.cursor()
     cur.execute("""SELECT * FROM states
