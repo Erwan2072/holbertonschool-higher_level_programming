f7e60dc0485940e2cf6bb54c91aa4949875d7766
Erwan2072 <erwan.lebreton@hotmail.fr>
2024-06-21 12:11:43 +0200

    correction

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -3,20 +3,26 @@
 script 14-model_city_fetch_by_state.py
 that prints all City objects from the database
 """
-from sys import argv
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 from model_city import City
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from sys import argv
 
 if __name__ == "__main__":
     engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1], argv[2], argv[3]),
+        "mysql+mysqldb://{}:{}@localhost/{}".format(argv[1], argv[2], argv[3]),
         pool_pre_ping=True,
-        )
+    )
     Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    cities = session.query(City).join(State).order_by(City.id).all()
-    for city in cities:
-        print("{}: ({}) {}".format(city.state.name, city.id, city.name))
+
+    session = Session(engine)
+    Query_cities_states = session\
+        .query(State, City)\
+        .join(City)\
+        .order_by(City.id.asc())
+
+    for state, city in Query_cities_states.all():
+        print(state.name + ': (' + str(city.id) + ') ' + city.name)
+
+    session.close()
