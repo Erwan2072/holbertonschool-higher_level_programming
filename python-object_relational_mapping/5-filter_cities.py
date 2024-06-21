b762d8ef2a518c2e2e1fcaf244743fae18781ed8
Erwan2072 <erwan.lebreton@hotmail.fr>
2024-06-21 17:13:49 +0200

    correction

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -7,14 +7,28 @@ import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=argv[1],
-                         passwd=argv[2], db=argv[3])
-    cur = db.cursor()
-    cur.execute("SELECT cities.name FROM cities"
-                "JOIN states ON cities.state_id = states.id"
-                "WHERE states.name = %s"
-                "ORDER BY cities.id ASC", (argv[4],))
-    rows = cur.fetchall()
-    print(", ".join([row[0] for row in rows]))
-    cur.close()
-    db.close()
+    connexion = MySQLdb.connect(
+        host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3])
+
+    argv_4 = argv[4].split(';')[0]
+
+    curs = connexion.cursor()
+    curs.execute("""
+                SELECT cities.name
+                FROM cities
+                INNER JOIN states ON cities.state_id = states.id
+                WHERE states.name = '{}'
+                ORDER BY cities.id ASC;
+                """.format(argv_4))
+
+    rows = curs.fetchall()
+    liste_cities = ()
+
+    if len(rows) > 0:
+        print(rows[0][0], end="")
+    for row in rows[1:]:
+        print(", " + row[0], end="")
+
+    print()
+    curs.close()
+    connexion.close()
